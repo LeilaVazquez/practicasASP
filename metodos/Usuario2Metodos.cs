fd80c67d4f564c0e90a70feb6ecb65135a4e0f4c
Leivaz <lei_vaz@hotmail.com>
2023-02-01 22:50:05 +0100

    Usuario2metodos

--- metodos/Usuario2Metodos.cs
+++ metodos/Usuario2Metodos.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using dominio;
+using metodos;
 
 namespace metodos
 {
@@ -17,13 +18,65 @@ namespace metodos
         public int insertarNuevo2(Usuario2 nuevo)
         {
             AccesoDatos datos = new AccesoDatos();
-
             try
             {
                 datos.setearProcedimiento("insertarNuevo2");
                 datos.setearParametro("@email", nuevo.Email);
                 datos.setearParametro("@pass", nuevo.Pass);
                 return datos.ejecutarAccionScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public bool Login(Usuario2 usuario2)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Select Id, Email, Pass, Admin, ImagenPerfil, Nombre, Apellido, FechaNacimiento from Usuarios2 where Email = @email and Pass = @pass"); // no deberia haber dos users con el mismo mail, esto tiene q devolver un solo registro
+                datos.setearParametro("@email", usuario2.Email);
+                datos.setearParametro("@pass", usuario2.Pass);
+                datos.ejecutarLectura();
+                if (datos.Lector.Read())
+                {
+                    usuario2.Id = (int)datos.Lector["Id"];
+                    usuario2.Admin = (bool)datos.Lector["Admin"];
+                    if (!(datos.Lector["ImagenPerfil"] is DBNull))
+                        usuario2.ImagenPerfil = (string)datos.Lector["ImagenPerfil"];
+                    if (!(datos.Lector["Nombre"] is DBNull))
+                        usuario2.Nombre = (string)datos.Lector["Nombre"];
+                    if (!(datos.Lector["Apellido"] is DBNull))
+                        usuario2.Apellido = (string)datos.Lector["Apellido"];
+                    if (!(datos.Lector["FechaNacimiento"] is DBNull))
+                        usuario2.FechaNacimiento = DateTime.Parse(datos.Lector["FechaNacimiento"].ToString()); //1ro a string y desp a DateTime
+                   return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void actualizar(Usuario2 user)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Update Usuarios2 set ImagenPerfil = @ImagenPerfil, Nombre = @nombre, Apellido = @apellido, FechaNacimiento = @fecha where Id = @id");
+               // datos.setearParametro("@ImagenPerfil", user.ImagenPerfil != null ? user.ImagenPerfil : (object)DBNull.Value);
+                datos.setearParametro("@ImagenPerfil", (object)user.ImagenPerfil ?? DBNull.Value); //null coalescing operator
+                datos.setearParametro("@nombre", user.Nombre);
+                datos.setearParametro("@apellido", user.Apellido);
+                datos.setearParametro("@fecha", user.FechaNacimiento);
+                datos.setearParametro("@id", user.Id); 
+                datos.ejecutarAccion();
 
             }
             catch (Exception ex)
@@ -35,32 +88,6 @@ namespace metodos
             {
                 datos.cerrarConexion();
             }
-
         }
-
-        public bool Login(Usuario2 usuario2)
-        {
-            AccesoDatos datos = new AccesoDatos();
-            try
-            {
-                datos.setearConsulta("Select Id, Email, Pass, Admin from Usuarios2 where Email = @email and Pass = @pass"); // no deberia haber dos users con el mismo mail, esto tiene q devolver un solo registro
-                datos.setearParametro("@email", usuario2.Email);
-                datos.setearParametro("@pass", usuario2.Pass);
-                datos.ejecutarLectura();
-                if (datos.Lector.Read())
-                {
-                    usuario2.Id = (int)datos.Lector["Id"];
-                    usuario2.Admin = (bool)datos.Lector["Admin"];
-                    return true;
-                }
-                return false;
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
-        }
-
     }
 }
